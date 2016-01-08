af875a4b6ee46b0e42bfb602208a085eee45ad1f
James Crockett <jecrockett@gmail.com>
2016-01-08 10:20:59 -0700

    Add functionality for creating an account

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name "MyString"
+password_digest "MyString"
+email "MyString"
+role 1
+  end
+
+end
--- spec/features/unregistered_user_creates_an_account_spec.rb
+++ spec/features/unregistered_user_creates_an_account_spec.rb
@@ -4,53 +4,61 @@ RSpec.feature "Unregistered user creates a new account" do
   scenario "they see the dashboard for their account" do
     email = "sally@fakeaccount.com"
     password = "sallyisthebest"
+    name = "Sally"
 
     visit new_user_path
+    fill_in "Name", with: name
     fill_in "Email", with: email
-
-    # coded tests using email as login instead of name, which might not be unique. Probably makes sense to have their email in our system anyway (it's super easy to use the Pony app to send emails, so let's trigger an email confirmation for successfully redeemed rewards.)
-
     fill_in "Password", with: password
     fill_in "Password confirmation", with: password
     click_on "Create Account"
 
-    expect(page).to have_content name
+    user = User.last
+    expect(page).to have_content user.name
+    expect(page).to have_content "Account created!"
   end
 
-#   context "the name is left blank" do
-#     password = "sallyisthebest"
-#
-#     visit new_user_path
-#     fill_in "Password", with: password
-#     fill_in "Password confirmation", with: password
-#     click_on "Create Account"
-#
-#     expect(page).to have_content "Please fill in all fields."
-#   end
-#
-#   context "the password confirmation is left blank" do
-#     email = "sally@fakeaccount.com"
-#
-#     visit new_user_path
-#     fill_in "Email", with: email
-#     fill_in "Password", with: password
-#     click_on "Create Account"
-#
-#     expect(page).to have_content "Please fill in all fields."
-#   end
-#
-#   context "the passwords do not match" do
-#     email = "sally@fakeaccount.com"
-#     password_1 = "sallyisthebest"
-#     password_2 = "sallyistheworst"
-#
-#     visit new_user_path
-#     fill_in "Email", with: email
-#     fill_in "Password", with: password_1
-#     fill_in "Password confirmation", with: password_2
-#     click_on "Create Account"
-#
-#     expect(page).to have_content "Your passwords do not match exactly. "
-#
-#   end
+  context "the name is left blank" do
+    scenario "they see an error message" do
+      password = "sallyisthebest"
+
+      visit new_user_path
+      fill_in "Password", with: password
+      fill_in "Password confirmation", with: password
+      click_on "Create Account"
+
+      expect(page).to have_content "Your email or password is incorrect."
+    end
+  end
+
+  context "the password confirmation is left blank" do
+    scenario "they see an error message" do
+      email = "sally@fakeaccount.com"
+      password = ""
+
+      visit new_user_path
+      fill_in "Email", with: email
+      fill_in "Password", with: password
+      fill_in "Password confirmation", with: password
+      click_on "Create Account"
+
+      expect(page).to have_content "Your email or password is incorrect."
+    end
+  end
+
+  context "the passwords do not match" do
+    scenario "they see an error message" do
+      email = "sally@fakeaccount.com"
+      password_1 = "sallyisthebest"
+      password_2 = "sallyistheworst"
+
+      visit new_user_path
+      fill_in "Email", with: email
+      fill_in "Password", with: password_1
+      fill_in "Password confirmation", with: password_2
+      click_on "Create Account"
+
+      expect(page).to have_content "Your email or password is incorrect."
+    end
+  end
 end
