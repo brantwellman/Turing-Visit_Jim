4ecfa8e2720a1dde7d5e220a3db1fc52e5df8050
James Crockett <jecrockett@gmail.com>
2016-01-09 12:15:36 -0700

    Add functionality for admin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :edit]
+
+  namespace :admin do
+    resources :users, only: [:index]
+  end
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+
+
 end
--- spec/features/registered_user_can_log_in_spec.rb
+++ spec/features/registered_user_can_log_in_spec.rb
@@ -14,20 +14,21 @@ RSpec.feature "Registered users can log into their account" do
     # add something unique for the show page here, like some CSS expectation once we get there. This would technically pass even if it loaded the user index or something if all we test for is the name existing.
   end
 
-  # scenario "admin user sees the dashboard for all regular users" do
-  #   admin = User.create(name: "Jim", email: "jim@fakeaccount.com", password: "bigjim", role: 1)
-  #   user_1 = User.create(name: "Sally", email: "sally@fakeaccount.com", password: "sallyisthebest")
-  #   user_2 = User.create(name: "Beth", email: "beth@fakeaccount.com", password: "littlemissbully")
-  #
-  #   visit login_path
-  #   fill_in "Email", with: admin.email
-  #   fill_in "Password", with: "bigjim"
-  #   click_button "Login"
-  #
-  #   expect(page).to have_content user_1.name
-  #   expect(page).to have_content user_2.name
-  # end
-  #
+  scenario "admin user sees the dashboard for all regular users" do
+    admin = User.create(name: "Jim", email: "jim@fakeaccount.com", password: "bigjim", role: 1)
+    user_1 = User.create(name: "Sally", email: "sally@fakeaccount.com", password: "sallyisthebest")
+    user_2 = User.create(name: "Beth", email: "beth@fakeaccount.com", password: "littlemissbully")
+
+    visit login_path
+    fill_in "Email", with: admin.email
+    fill_in "Password", with: "bigjim"
+    click_button "Login"
+
+    expect(current_path).to eq admin_users_path
+    expect(page).to have_content user_1.name
+    expect(page).to have_content user_2.name
+  end
+
   context "they leave the name field blank" do
     scenario "they see an error message" do
       user = User.create(name: "Sally", email: "sally@fakeaccount.com", password: "sallyisthebest")
