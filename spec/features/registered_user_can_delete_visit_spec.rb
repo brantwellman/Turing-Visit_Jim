29d232e90615c73b3e26e54e1e4ecaf7d85753fb
Brant Wellman <BrantWellman@gmail.com>
2016-01-09 16:27:18 -0700

    Add delete visit functionality

--- spec/features/registered_user_can_delete_visit_spec.rb
+++ spec/features/registered_user_can_delete_visit_spec.rb
@@ -1,15 +1,16 @@
-# require 'rails_helper'
-#
-# RSpec.feature "Registered user deletes a gym visit" do
-#   scenario "they see the index with their user page without visit info" do
-#
-#     user = create(:user_with_gymvisits)
-#     login_user
-#
-#     click_on "Delete Visit"
-#
-#     expect(current_path).to eq user_path(user)
-#     expect(page).to have_content user.name
-#     expect(page).to have_content "2000"
-#   end
-# end
+require 'rails_helper'
+
+RSpec.feature "Registered user deletes a gym visit" do
+  scenario "they see the index with their user page without visit info" do
+    user = create(:user_with_gymvisit)
+    login_user(user)
+
+    expect(page).to have_content "01/01/2016"
+
+    click_on "Delete Visit"
+
+    expect(current_path).to eq user_path(user)
+    expect(page).to have_content user.name
+    expect(page).to_not have_content "01/01/2016"
+  end
+end
