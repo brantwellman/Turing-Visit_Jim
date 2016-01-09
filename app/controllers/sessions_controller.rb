77e72a376c92d46e33568905d0f8b3ab18cec5e3
Brant Wellman <BrantWellman@gmail.com>
2016-01-09 11:35:18 -0700

    Add user login functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      flash[:error] = "Invalid email or password."
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :edit]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 end
