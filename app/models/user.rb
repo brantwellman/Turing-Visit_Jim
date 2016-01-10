2f26de95018d3f21ef5e8af8f6aa6d591db7a567
Brant Wellman <BrantWellman@gmail.com>
2016-01-10 13:42:07 -0700

    Allow admin to update user points

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
 
   enum role: %w(default admin)
+
+  def update_total_points(redeemable_points)
+    new_points = redeemable_points.to_i - self.total_points
+    self.total_points = self.total_points + new_points
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  resources :users, only: [:new, :create, :show, :edit] do
+  resources :users, only: [:new, :create, :show] do
     resources :gymvisits, only: [:new, :create, :edit, :update, :destroy]
   end
 
   namespace :admin do
-    resources :users, only: [:index, :show]
+    resources :users, only: [:index, :show, :edit, :update]
     resources :gymvisits, only: [:edit, :update, :destroy]
   end
 
